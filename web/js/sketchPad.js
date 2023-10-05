7316f9cb01e21b9388112da917a444de148b3155
EhsanTgv <EhsanTgv@gmail.com>
2023-10-05 17:55:59 +0300

    Add touch support and add undo button

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -9,6 +9,13 @@ class SketchPad {
         `;
         container.appendChild(this.canvas);
 
+        const lineBreak = document.createElement("br");
+        container.appendChild(lineBreak);
+
+        this.undoBtn = document.createElement("button");
+        this.undoBtn.innerHTML = "UNDO";
+        container.appendChild(this.undoBtn);
+
         this.ctx = this.canvas.getContext("2d");
 
         this.paths = [];
@@ -26,13 +33,28 @@ class SketchPad {
         this.canvas.onmousemove = (evt) => {
             if (this.isDrawing) {
                 const mouse = this.#getMouse(evt);
-                const lastPath = this.paths[this.paths.length-1];
+                const lastPath = this.paths[this.paths.length - 1];
                 lastPath.push(mouse);
                 this.#redraw();
             }
         }
         this.canvas.onmouseup = () => {
             this.isDrawing = false;
+        }
+        this.canvas.ontouchstart = (evt) => {
+            const loc = evt.touches[0];
+            this.canvas.onmousedown(loc);
+        }
+        this.canvas.ontouchmove = (evt) => {
+            const loc = evt.touches[0];
+            this.canvas.onmousemove(loc);
+        }
+        this.canvas.ontouchend = () => {
+            this.canvas.onmouseup();
+        }
+        this.undoBtn.onclick = () => {
+            this.paths.pop();
+            this.#redraw();
         }
     }
 
